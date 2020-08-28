f470c1ca89b26fc4985e66ed0597c01ece14d1bd
nmhahn <nicolasmahahn@gmail.com>
2020-08-28 09:27:04 -0300

    Update

--- CV/dados.R
+++ CV/dados.R
@@ -10,22 +10,12 @@ edu = tribble(
 # experiência profissional
 experiencia = tribble(
   ~cargo, ~inicio_mes, ~inicio_ano, ~fim_mes, ~fim_ano, ~empresa, ~detalhe,
+  "Bolsista de Iniciação Científica", "Agosto", 2019, "", "momento", "Universidade Federal do Rio Grande do Sul", "Análise de Sobrevivência",
   "Cientista de Dados", "Julho", 2020, "Agosto", "2020", "3ECT - Inteligência em Dados", "Previsão de Demanda",
   "Cientista de Dados", "Julho", 2020, "Agosto", "2020", "3ECT - Inteligência em Dados", "Controle de Estoque",
   "Cientista de Dados", "Março", 2020, "Maio", "2020", "3ECT - Inteligência em Dados", "Análise de Vendas (produtos mais vendidos, quais produtos são vendidos em conjunto)",
-  "Cientista de Dados", "Março", 2020, "Maio", "2020", "3ECT - Inteligência em Dados", "Criação de Relatórios",
-  "Bolsista de Iniciação Científica", "Agosto", 2019, "", "momento", "Universidade Federal do Rio Grande do Sul", "Análise de Sobrevivência"
+  "Cientista de Dados", "Março", 2020, "Maio", "2020", "3ECT - Inteligência em Dados", "Criação de Relatórios"
 )
-
-
-# experiencia = tribble(
-#   ~cargo, ~inicio_mes, ~inicio_ano, ~fim_mes, ~fim_ano, ~empresa, ~detalhe,
-#   "Cientista de Dados", "Julho", 2020, "Agosto", 2020, "3ECT - Inteligência em Dados", "Previsão de Demanda",
-#   "Cientista de Dados", "Julho", 2020, "Agosto", 2020, "3ECT - Inteligência em Dados", "Controle de Estoque",
-#   "Cientista de Dados", "Março", 2020, "Maio", 2020, "3ECT - Inteligência em Dados", "Análise de Vendas (produtos mais vendidos, quais produtos são vendidos em conjunto)",
-#   "Cientista de Dados", "Março", 2020, "Maio", 2020, "3ECT - Inteligência em Dados", "Criação de Relatórios",
-#   "Bolsista de Iniciação Científica", "Agosto", 2019, NA, "momento", "Universidade Federal do Rio Grande do Sul", "Análise de Sobrevivência" 
-# )
 
 # habilidades
 habilidades = tribble(
@@ -34,13 +24,14 @@ habilidades = tribble(
   "Analítico", "Modelagem Estatística, Ciência de Dados",
   "Linguagens de Programação", "R (avançado), Python, SQL, MATLAB, Fortran",
   "Linguagens de Marcação (Markup)", "LaTeX, Markdown, RMarkdown",
-  "Ferramentas", "Excel, Power BI, Tableau, Git",
+  "Ferramentas", "Excel, Pacote Office, Power BI, Tableau, Git",
   "Idiomas", "Português (nativo), Inglês (avançado), Espanhol (iniciante)"
 )
 
 # cursos (e treinamentos)
 cursos = tribble(
   ~instituicao, ~curso, ~mes, ~ano, ~detalhe,
+  "Udemy", "Análise Técnica e Inteligência Artificial para Investimentos", "Agosto", 2020, "Online",
   "Udemy", "Curso de Vendas Completo - Da Prospecção ao Fechamento", "Agosto", 2020, "Online",
   "Udemy", "Formação Cientista de Dados com Python e R", "Junho", 2020, "Online",
   "Escola Superior de Propaganda e Marketing - ESPM", "Business Analytics: Análise de Dados para Tomada de Decisão", "Fevereiro", 2020, "Presencial",
