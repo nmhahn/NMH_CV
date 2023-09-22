f7189e4f5fa9034c9f50061fd2ad52b429b53b78
nmhahn <nicolasmahahn@gmail.com>
2023-09-22 13:57:09 -0300

    Update CV - Data Science Jr Sicredi

--- CV/data-enUS.R
+++ CV/data-enUS.R
@@ -4,69 +4,74 @@ options(encoding = "UTF-8")
 edu = tribble(
   ~formacao, ~inicio, ~fim, ~instituicao, ~detalhe,
   "Statistics (Bachelor's Degree)", 2018, 2022, "Federal University of Rio Grande do Sul - UFRGS", NA,
-  "High School", 2014, 2016, "Marista Champagnat School", NA,
+  "High School"                   , 2014, 2016, "Marista Champagnat School"                      , NA,
 )
 
 # experiência profissional
 experiencia = tribble(
   ~cargo, ~inicio_mes, ~inicio_ano, ~fim_mes, ~fim_ano, ~empresa, ~detalhe,
-  "Business Intelligence Analyst (Sales Ops)", "January", 2022, "", "moment", "Aegro", "Creating Reports and Dashboards with Power BI and R",
-  "Business Intelligence Analyst (Sales Ops)", "January", 2022, "", "moment", "Aegro", "Helped develop the Sales Department Structure in Power BI",
-  "Business Intelligence Analyst (Sales Ops)", "January", 2022, "", "moment", "Aegro", "Automatizations with Power Automate (report update, sending emails)",
-  "Business Intelligence Analyst (Sales Ops)", "January", 2022, "", "moment", "Aegro", "Ensuring Data Reliability",
-  "Business Intelligence Analyst (Sales Ops)", "January", 2022, "", "moment", "Aegro", "Sales Operation KPI's Monitoring",
-  "Business Intelligence Analyst (Sales Ops)", "January", 2022, "", "moment", "Aegro", "Presentation and Documentation of the Material Produced (reports, analysis)",
-  "Data Science Intern (Sales Ops)", "February", 2021, "January", "2022", "Aegro", "Creating Reports and Dashboards with Power BI and R",
-  "Data Science Intern (Sales Ops)", "February", 2021, "January", "2022", "Aegro", "Ensuring Data Reliability",
-  "Data Science Intern (Sales Ops)", "February", 2021, "January", "2022", "Aegro", "Sales Operation KPI's Monitoring",
-  "Data Science Intern (Sales Ops)", "February", 2021, "January", "2022", "Aegro", "Presentation and Documentation of the Material Produced",
-  "Scientific Initiation Scholarship", "August", 2019, "March", "2021", "Federal University of Rio Grande do Sul - UFRGS", "Field: Survival Analysis",
-  "Scientific Initiation Scholarship", "August", 2019, "March", "2021", "Federal University of Rio Grande do Sul - UFRGS", "Project: Models for Survival Data with Informational Censorship",
-  "Scientific Initiation Scholarship", "August", 2019, "March", "2021", "Federal University of Rio Grande do Sul - UFRGS", "Activities: simulations, literature review, programming languages comparison, applications and results analysis.",
-  "Data Scientist", "July", 2020, "August", "2020", "3ECT - Inteligência em Dados", "Demand Forecast",
-  "Data Scientist", "July", 2020, "August", "2020", "3ECT - Inteligência em Dados", "Inventory Control",
-  "Data Scientist", "March", 2020, "May", "2020", "3ECT - Inteligência em Dados", "Market Basket Analysis",
-  "Data Scientist", "March", 2020, "May", "2020", "3ECT - Inteligência em Dados", "Report Creation"
+  "Data Scientist"                             , "January" , 2023, ""        , "moment", "Sicredi"                                        , "Natural Language Processing (NLP) applied to News for Insights",
+  "Data Scientist"                             , "January" , 2023, ""        , "moment", "Sicredi"                                        , "Implementing Process Automation to Streamline KPI Calculations",
+  "Data Scientist"                             , "January" , 2023, ""        , "moment", "Sicredi"                                        , "Monitoring the KPI's of the Executive Credit Board (DEC)",
+  "Business Intelligence Analyst (Sales Ops)"  , "January" , 2022, "January" , "2023"  , "Aegro"                                          , "Structuring the Tables/Views consumed by the Sales team (results and goals of the operation)",  
+  "Business Intelligence Analyst (Sales Ops)"  , "January" , 2022, "January" , "2023"  , "Aegro"                                          , "Development of the Sales Department Structure in Power BI",
+  "Business Intelligence Analyst (Sales Ops)"  , "January" , 2022, "January" , "2023"  , "Aegro"                                          , "Automatizations with Power Automate (report update, sending emails)",
+  "Business Intelligence Analyst (Sales Ops)"  , "January" , 2022, "January" , "2023"  , "Aegro"                                          , "Creating Reports and Dashboards with Power BI and R",
+  "Business Intelligence Analyst (Sales Ops)"  , "January" , 2022, "January" , "2023"  , "Aegro"                                          , "Ensuring Data Reliability",
+  "Business Intelligence Analyst (Sales Ops)"  , "January" , 2022, "January" , "2023"  , "Aegro"                                          , "Sales Operation KPI's Monitoring",
+  "Business Intelligence Analyst (Sales Ops)"  , "January" , 2022, "January" , "2023"  , "Aegro"                                          , "Presentation and Documentation of the Material Produced (reports, analysis)",
+  "Data Science Intern (Sales Ops)"            , "February", 2021, "January" , "2022"  , "Aegro"                                          , "Creating Reports and Dashboards with Power BI and R",
+  "Data Science Intern (Sales Ops)"            , "February", 2021, "January" , "2022"  , "Aegro"                                          , "Ensuring Data Reliability",
+  "Data Science Intern (Sales Ops)"            , "February", 2021, "January" , "2022"  , "Aegro"                                          , "Sales Operation KPI's Monitoring",
+  "Data Science Intern (Sales Ops)"            , "February", 2021, "January" , "2022"  , "Aegro"                                          , "Presentation and Documentation of the Material Produced",
+  "Scientific Initiation Scholarship"          , "August"  , 2019, "March"   , "2021"  , "Federal University of Rio Grande do Sul - UFRGS", "Field: Survival Analysis",
+  "Scientific Initiation Scholarship"          , "August"  , 2019, "March"   , "2021"  , "Federal University of Rio Grande do Sul - UFRGS", "Project: Models for Survival Data with Informational Censorship",
+  "Scientific Initiation Scholarship"          , "August"  , 2019, "March"   , "2021"  , "Federal University of Rio Grande do Sul - UFRGS", "Activities: simulations, literature review, programming languages comparison, applications and results analysis.",
+  # "Data Scientist"                             , "July"    , 2020, "August"  , "2020"  , "3ECT - Inteligência em Dados"                   , "Demand Forecast",
+  # "Data Scientist"                             , "July"    , 2020, "August"  , "2020"  , "3ECT - Inteligência em Dados"                   , "Inventory Control",
+  # "Data Scientist"                             , "March"   , 2020, "May"     , "2020"  , "3ECT - Inteligência em Dados"                   , "Market Basket Analysis",
+  # "Data Scientist"                             , "March"   , 2020, "May"     , "2020"  , "3ECT - Inteligência em Dados"                   , "Report Creation"
 )
 
 # habilidades
 habilidades = tribble(
   ~area, ~habilidades,
-  "Comunication", "Speaking, Writing, Reports and Dashboards",
-  "Analytics", "Dimensional and Statistical Modeling, Data Analysis and Data Science",
+  "Comunication"         , "Speaking, Writing, Reports and Dashboards",
+  "Analytics"            , "Dimensional and Statistical Modeling, Data Analysis and Data Science",
   "Programming Languages", "R (advanced), Python, SQL, MATLAB, Fortran",
-  "Markup Languages", "LaTeX, Markdown, RMarkdown",
-  "Tools", "Power BI (DAX, M Query), Git, Power Automate, Excel, Office, Tableau",
-  "Languages", "Portuguese (native), English (C2), Spanish (basic)"
+  "Markup Languages"     , "LaTeX, Markdown, RMarkdown",
+  "Tools"                , "Power BI (DAX, M Query), Git, Power Automate, Excel, Office, Tableau",
+  "Languages"            , "Portuguese (native), English (C2), Spanish (basic)"
 )
 
 # cursos (e treinamentos)
 cursos = tribble(
   ~instituicao, ~curso, ~mes, ~ano, ~detalhe,
-  "EF SET (Online)", "Education First Standard English Test", "August", 2022, NA,
-  "Escola Conquer (Online)", "Negociação e Influência", "November", 2021, NA,
-  "Data Science Academy - DSA (Online)", "Python Fundamentos para Análise de Dados (Versão 3.0)", "November", 2021, NA,
-  "Data Science Academy - DSA (Online)", "Microsoft Power BI para Data Science (Versão 2.0)", "June", 2021, NA,
-  "Udemy (Online)", "Análise Técnica e Inteligência Artificial para Investimentos", "August", 2020, NA,
-  "Udemy (Online)", "Curso de Vendas Completo - Da Prospecção ao Fechamento", "August", 2020, NA,
-  "Udemy (Online)", "Formação Cientista de Dados com Python e R", "June", 2020, NA,
-  "Escola Superior de Propaganda e Marketing - ESPM (On-site)", "Business Analytics: Análise de Dados para Tomada de Decisão", "February", 2020, NA,
-  "Universidade Federal do Rio Grande do Sul - UFRGS (On-site)", "Excel para Iniciantes", "July", 2018, NA
+  "EF SET (Online)"                                            , "Education First Standard English Test", "August", 2022, NA,
+  "Escola Conquer (Online)"                                    , "Negociação e Influência", "November", 2021, NA,
+  "Data Science Academy - DSA (Online)"                        , "Python Fundamentos para Análise de Dados (Versão 3.0)", "November", 2021, NA,
+  "Data Science Academy - DSA (Online)"                        , "Microsoft Power BI para Data Science (Versão 2.0)", "June", 2021, NA,
+  "Udemy (Online)"                                             , "Análise Técnica e Inteligência Artificial para Investimentos", "August", 2020, NA,
+  "Udemy (Online)"                                             , "Curso de Vendas Completo - Da Prospecção ao Fechamento", "August", 2020, NA,
+  "Udemy (Online)"                                             , "Formação Cientista de Dados com Python e R", "June", 2020, NA,
+  "College of Advertising and Marketing - ESPM (On-site)"      , "Business Analytics: Análise de Dados para Tomada de Decisão", "February", 2020, NA,
+  "Federal University of Rio Grande do Sul - UFRGS (On-site)"  , "Excel para Iniciantes", "July", 2018, NA
 )
 
 # eventos participados
 eventos = tribble(
   ~instituicao, ~evento, ~mes, ~ano, ~detalhe,
-  "Google Cloud (Online)", "Big Data & Machine Learning", "June", 2019, NA,
-  "Pontifícia Universidade Católica do Rio Grande do Sul - PUCRS (On-site)", "Feira de Ciências e Inovação do Museu de Ciências e Tecnologia da PUCRS", "October", 2016, NA,
-  "Universidade Federal do Rio Grande do Sul - UFRGS (On-site)", "XI Salão UFRGS Jovem", "September", 2016, NA
+  "Federal University of Bahia - UFBA (Online)"                  , "4th Conference on Statistics and Data Science"                          , "December" , 2022, NA,
+  "Google Cloud (Online)"                                        , "Big Data & Machine Learning"                                            , "June"     , 2019, NA,
+  "Pontifical Catholic University of Rio Grande do Sul (On-site)", "Feira de Ciências e Inovação do Museu de Ciências e Tecnologia da PUCRS", "October"  , 2016, NA,
+  "Federal University of Rio Grande do Sul - UFRGS (On-site)"    , "XI Salão UFRGS Jovem"                                                   , "September", 2016, NA
 )
 
 # hobbies
 hobbies = tribble(
   ~hobby, ~detalhe,
-  "Games", "Metroidvania, Platform, Souls Like, Survival Horror",
-  "Chess", "Medalist in Amateur Championships",
+  "Games"      , "Metroidvania, Platform, Souls Like, Survival Horror",
+  "Chess"      , "Medalist in Amateur Championships",
   "Investments", "Fixed and Variable Income (stocks)",
-  "Music", "Violin, Orchestra, Singing"
+  "Music"      , "Violin, Orchestra, Singing"
 )
--- CV/data-ptBR.R
+++ CV/data-ptBR.R
@@ -4,69 +4,74 @@ options(encoding = "UTF-8")
 edu = tribble(
   ~formacao, ~inicio, ~fim, ~instituicao, ~detalhe,
   "Estatística (Bacharelado)", 2018, 2022, "Universidade Federal do Rio Grande do Sul - UFRGS", NA,
-  "Ensino Médio", 2014, 2016, "Colégio Marista Champagnat", NA,
+  "Ensino Médio"             , 2014, 2016, "Colégio Marista Champagnat",                        NA
 )
 
 # experiência profissional
 experiencia = tribble(
   ~cargo, ~inicio_mes, ~inicio_ano, ~fim_mes, ~fim_ano, ~empresa, ~detalhe,
-  "Analista de BI (Sales Ops)", "Janeiro", 2022, "", "momento", "Aegro", "Criação de Relatórios e Dashboards",
-  "Analista de BI (Sales Ops)", "Janeiro", 2022, "", "momento", "Aegro", "Auxiliei no Desenvolvimento da Estrutura do Departamento de Vendas no Power BI",
-  "Analista de BI (Sales Ops)", "Janeiro", 2022, "", "momento", "Aegro", "Automatizações com Power Automate (atualizações de relatórios, envios de e-mail)",
-  "Analista de BI (Sales Ops)", "Janeiro", 2022, "", "momento", "Aegro", "Garantir a Confiabilidade dos Dados",
-  "Analista de BI (Sales Ops)", "Janeiro", 2022, "", "momento", "Aegro", "Monitoramento de KPI's da Operação de Vendas",
-  "Analista de BI (Sales Ops)", "Janeiro", 2022, "", "momento", "Aegro", "Apresentação e Documentação do Material Produzido",
-  "Estágio em Ciência de Dados (Sales Ops)", "Fevereiro", 2021, "Janeiro", "2022", "Aegro", "Criação de Relatórios e Dashboards",
-  "Estágio em Ciência de Dados (Sales Ops)", "Fevereiro", 2021, "Janeiro", "2022", "Aegro", "Garantir a Confiabilidade dos Dados",
-  "Estágio em Ciência de Dados (Sales Ops)", "Fevereiro", 2021, "Janeiro", "2022", "Aegro", "Monitoramento de KPI's da Operação de Vendas",
-  "Estágio em Ciência de Dados (Sales Ops)", "Fevereiro", 2021, "Janeiro", "2022", "Aegro", "Apresentação e Documentação do Material Produzido",
-  "Bolsista de Iniciação Científica", "Agosto", 2019, "Março", "2021", "Universidade Federal do Rio Grande do Sul - UFRGS", "Área: Análise de Sobrevivência",
-  "Bolsista de Iniciação Científica", "Agosto", 2019, "Março", "2021", "Universidade Federal do Rio Grande do Sul - UFRGS", "Projeto: Modelos para Dados de Sobrevivência com Censura Informativa",
-  "Bolsista de Iniciação Científica", "Agosto", 2019, "Março", "2021", "Universidade Federal do Rio Grande do Sul - UFRGS", "Atividades: simulações, revisão da literatura, comparativo de linguagens, aplicações e análise de resultados.",
-  "Cientista de Dados", "Julho", 2020, "Agosto", "2020", "3ECT - Inteligência em Dados", "Previsão de Demanda",
-  "Cientista de Dados", "Julho", 2020, "Agosto", "2020", "3ECT - Inteligência em Dados", "Controle de Estoque",
-  "Cientista de Dados", "Março", 2020, "Maio", "2020", "3ECT - Inteligência em Dados", "Análise de Cesta de Mercado",
-  "Cientista de Dados", "Março", 2020, "Maio", "2020", "3ECT - Inteligência em Dados", "Criação de Relatórios"
+  "Cientista de Dados"                     , "Janeiro"  , 2023, ""         , "momento", "Sicredi"                                          , "Processamento de Linguagem Natural (NLP) aplicada a Notícias para Obtenção de Insights",
+  "Cientista de Dados"                     , "Janeiro"  , 2023, ""         , "momento", "Sicredi"                                          , "Automação de Processos para Cálculos de KPI's",
+  "Cientista de Dados"                     , "Janeiro"  , 2023, ""         , "momento", "Sicredi"                                          , "Acompanhamento dos KPI's da Diretoria Executiva de Crédito",
+  "Analista de BI (Sales Ops)"             , "Janeiro"  , 2022, "Janeiro"  , "2023"   , "Aegro"                                            , "Estruturação das Tabelas/Views consumidas pelo time de Vendas (resultados e metas da operação)",
+  "Analista de BI (Sales Ops)"             , "Janeiro"  , 2022, "Janeiro"  , "2023"   , "Aegro"                                            , "Desenvolvimento da Estrutura do Departamento de Vendas no Power BI",
+  "Analista de BI (Sales Ops)"             , "Janeiro"  , 2022, "Janeiro"  , "2023"   , "Aegro"                                            , "Automatizações com Power Automate (atualizações de relatórios, envios de e-mail)",
+  "Analista de BI (Sales Ops)"             , "Janeiro"  , 2022, "Janeiro"  , "2023"   , "Aegro"                                            , "Criação de Relatórios e Dashboards",
+  "Analista de BI (Sales Ops)"             , "Janeiro"  , 2022, "Janeiro"  , "2023"   , "Aegro"                                            , "Garantir a Confiabilidade dos Dados",
+  "Analista de BI (Sales Ops)"             , "Janeiro"  , 2022, "Janeiro"  , "2023"   , "Aegro"                                            , "Monitoramento de KPI's da Operação de Vendas",
+  "Analista de BI (Sales Ops)"             , "Janeiro"  , 2022, "Janeiro"  , "2023"   , "Aegro"                                            , "Apresentação e Documentação do Material Produzido",
+  "Estágio em Ciência de Dados (Sales Ops)", "Fevereiro", 2021, "Janeiro"  , "2022"   , "Aegro"                                            , "Criação de Relatórios e Dashboards",
+  "Estágio em Ciência de Dados (Sales Ops)", "Fevereiro", 2021, "Janeiro"  , "2022"   , "Aegro"                                            , "Garantir a Confiabilidade dos Dados",
+  "Estágio em Ciência de Dados (Sales Ops)", "Fevereiro", 2021, "Janeiro"  , "2022"   , "Aegro"                                            , "Monitoramento de KPI's da Operação de Vendas",
+  "Estágio em Ciência de Dados (Sales Ops)", "Fevereiro", 2021, "Janeiro"  , "2022"   , "Aegro"                                            , "Apresentação e Documentação do Material Produzido",
+  "Bolsista de Iniciação Científica"       , "Agosto"   , 2019, "Março"    , "2021"   , "Universidade Federal do Rio Grande do Sul - UFRGS", "Área: Análise de Sobrevivência",
+  "Bolsista de Iniciação Científica"       , "Agosto"   , 2019, "Março"    , "2021"   , "Universidade Federal do Rio Grande do Sul - UFRGS", "Projeto: Modelos para Dados de Sobrevivência com Censura Informativa",
+  "Bolsista de Iniciação Científica"       , "Agosto"   , 2019, "Março"    , "2021"   , "Universidade Federal do Rio Grande do Sul - UFRGS", "Atividades: simulações, revisão da literatura, comparativo de linguagens, aplicações e análise de resultados.",
+  # "Cientista de Dados"                     , "Julho"    , 2020, "Agosto"   , "2020"   , "3ECT - Inteligência em Dados"                     , "Previsão de Demanda",
+  # "Cientista de Dados"                     , "Julho"    , 2020, "Agosto"   , "2020"   , "3ECT - Inteligência em Dados"                     , "Controle de Estoque",
+  # "Cientista de Dados"                     , "Março"    , 2020, "Maio"     , "2020"   , "3ECT - Inteligência em Dados"                     , "Análise de Cesta de Mercado",
+  # "Cientista de Dados"                     , "Março"    , 2020, "Maio"     , "2020"   , "3ECT - Inteligência em Dados"                     , "Criação de Relatórios"
 )
 
 # habilidades
 habilidades = tribble(
   ~area, ~habilidades,
-  "Comunicação", "Oral, Escrita, Criação de Relatórios e Dashboards",
-  "Analítico", "Modelagem Dimensional e Estatística, Análise e Ciência de Dados",
-  "Linguagens de Programação", "R (avançado), Python, SQL, MATLAB, Fortran",
+  "Comunicação"                    , "Oral, Escrita, Criação de Relatórios e Dashboards",
+  "Analítico"                      , "Modelagem Dimensional e Estatística, Análise e Ciência de Dados",
+  "Linguagens de Programação"      , "R (avançado), Python, SQL, MATLAB, Fortran",
   "Linguagens de Marcação (Markup)", "LaTeX, Markdown, RMarkdown",
-  "Ferramentas", "Power BI (DAX, M Query), Git, Power Automate, Excel, Pacote Office, Tableau",
-  "Idiomas", "Português (nativo), Inglês (avançado), Espanhol (iniciante)"
+  "Ferramentas"                    , "Power BI (DAX, M Query), Git, Power Automate, Excel, Pacote Office, Tableau",
+  "Idiomas"                        , "Português (nativo), Inglês (avançado), Espanhol (iniciante)"
 )
 
 # cursos (e treinamentos)
 cursos = tribble(
   ~instituicao, ~curso, ~mes, ~ano, ~detalhe,
-  "EF SET (Online)", "Education First Standard English Test", "Agosto", 2022, NA,
-  "Escola Conquer (Online)", "Negociação e Influência", "Novembro", 2021, NA,
-  "Data Science Academy - DSA (Online)", "Python Fundamentos para Análise de Dados (Versão 3.0)", "Novembro", 2021, NA,
-  "Data Science Academy - DSA (Online)", "Microsoft Power BI para Data Science (Versão 2.0)", "Junho", 2021, NA,
-  "Udemy (Online)", "Análise Técnica e Inteligência Artificial para Investimentos", "Agosto", 2020, NA,
-  "Udemy (Online)", "Curso de Vendas Completo - Da Prospecção ao Fechamento", "Agosto", 2020, NA,
-  "Udemy (Online)", "Formação Cientista de Dados com Python e R", "Junho", 2020, NA,
-  "Escola Superior de Propaganda e Marketing - ESPM (Presencial)", "Business Analytics: Análise de Dados para Tomada de Decisão", "Fevereiro", 2020, NA,
-  "Universidade Federal do Rio Grande do Sul - UFRGS (Presencial)", "Excel para Iniciantes", "Julho", 2018, NA
+  "EF SET (Online)"                                               , "Education First Standard English Test"                       , "Agosto"   , 2022, NA,
+  "Escola Conquer (Online)"                                       , "Negociação e Influência"                                     , "Novembro" , 2021, NA,
+  "Data Science Academy - DSA (Online)"                           , "Python Fundamentos para Análise de Dados (Versão 3.0)"       , "Novembro" , 2021, NA,
+  "Data Science Academy - DSA (Online)"                           , "Microsoft Power BI para Data Science (Versão 2.0)"           , "Junho"    , 2021, NA,
+  "Udemy (Online)"                                                , "Análise Técnica e Inteligência Artificial para Investimentos", "Agosto"   , 2020, NA,
+  "Udemy (Online)"                                                , "Curso de Vendas Completo - Da Prospecção ao Fechamento"      , "Agosto"   , 2020, NA,
+  "Udemy (Online)"                                                , "Formação Cientista de Dados com Python e R"                  , "Junho"    , 2020, NA,
+  "Escola Superior de Propaganda e Marketing - ESPM (Presencial)" , "Business Analytics: Análise de Dados para Tomada de Decisão" , "Fevereiro", 2020, NA,
+  "Universidade Federal do Rio Grande do Sul - UFRGS (Presencial)", "Excel para Iniciantes"                                       , "Julho"    , 2018, NA
 )
 
 # eventos participados
 eventos = tribble(
   ~instituicao, ~evento, ~mes, ~ano, ~detalhe,
-  "Google Cloud (Online)", "Big Data & Machine Learning", "Junho", 2019, NA,
-  "Pontifícia Universidade Católica do Rio Grande do Sul - PUCRS (Presencial)", "Feira de Ciências e Inovação do Museu de Ciências e Tecnologia da PUCRS", "Outubro", 2016, NA,
-  "Universidade Federal do Rio Grande do Sul - UFRGS (Presencial)", "XI Salão UFRGS Jovem", "Setembro", 2016, NA
+  "Universidade Federal da Bahia - UFBA (Online)"                             , "4ª Conferência em Estatística e em Ciência de Dados"                    , "Dezembro" , 2022, NA,
+  "Google Cloud (Online)"                                                     , "Big Data & Machine Learning"                                            , "Junho"    , 2019, NA,
+  "Pontifícia Universidade Católica do Rio Grande do Sul - PUCRS (Presencial)", "Feira de Ciências e Inovação do Museu de Ciências e Tecnologia da PUCRS", "Outubro"  , 2016, NA,
+  "Universidade Federal do Rio Grande do Sul - UFRGS (Presencial)"            , "XI Salão UFRGS Jovem"                                                   , "Setembro" , 2016, NA
 )
 
 # hobbies
 hobbies = tribble(
   ~hobby, ~detalhe,
-  "Games", "Metroidvania, Plataforma, Souls Like, Survival Horror",
-  "Xadrez", "Medalhista em Campeonatos Amadores",
+  "Games"        , "Metroidvania, Plataforma, Souls Like, Survival Horror",
+  "Xadrez"       , "Medalhista em Campeonatos Amadores",
   "Investimentos", "Renda Fixa (Tesouro Direto), Renda Variável (Ações, FII's)",
-  "Música", "Violino, Prática de Orquestra, Canto"
+  "Música"       , "Violino, Prática de Orquestra, Canto"
 )
